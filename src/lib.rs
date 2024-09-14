2e6ceafa29ea7efd0835ab8d8bbe13d2e116e8b2
filipton <filipton12@gmail.com>
2024-09-14 22:07:19 +0200

    feat: value unsafe pointers to parent (for easy shifter updates)

--- src/lib.rs
+++ src/lib.rs
@@ -21,37 +21,22 @@ impl<const N: usize, OP: OutputPin> AdvancedShiftRegister<N, OP> {
         }
     }
 
-    pub fn get_shifter_mut(&mut self, i: usize) -> ShifterValue {
+    pub fn get_shifter_mut(&mut self, i: usize) -> ShifterValue<N, OP> {
         ShifterValue {
             inner: core::ptr::addr_of_mut!(self.shifters[i]),
+            parent: core::ptr::addr_of_mut!(*self),
         }
     }
 
-    pub fn get_shifter_range_mut(&mut self, range: Range<usize>) -> ShifterValueRange {
-        let len = range.len();
+    pub fn get_shifter_range_mut(&mut self, range: Range<usize>) -> ShifterValueRange<N, OP> {
         ShifterValueRange {
+            //len: range.len(),
             inner: core::ptr::addr_of_mut!(self.shifters[range]),
-            len,
+            parent: core::ptr::addr_of_mut!(*self),
         }
     }
 
-    pub fn shift_test(&mut self, mut val: u8, latch: bool) {
-        for _ in 0..8 {
-            let state = PinState::from(val & 1 > 0);
-            _ = self.data_pin.set_state(state);
-            val >>= 1;
-
-            _ = self.clk_pin.set_high();
-            _ = self.clk_pin.set_low();
-        }
-
-        if latch {
-            _ = self.latch_pin.set_high();
-            _ = self.latch_pin.set_low();
-        }
-    }
-
-    pub fn shift_test_2(&mut self) {
+    pub fn update_shifters(&mut self) {
         for i in (0..N).rev() {
             let mut val = self.shifters[i];
 
@@ -71,25 +56,33 @@ impl<const N: usize, OP: OutputPin> AdvancedShiftRegister<N, OP> {
 }
 
 #[derive(Clone)]
-pub struct ShifterValue {
+pub struct ShifterValue<const N: usize, OP: OutputPin> {
     inner: *mut u8,
+    parent: *mut AdvancedShiftRegister<N, OP>,
 }
 
-impl ShifterValue {
+impl<const N: usize, OP: OutputPin> ShifterValue<N, OP> {
     pub fn set_value(&self, value: u8) {
         unsafe {
             *self.inner = value;
         }
     }
+
+    pub fn update_shifters(&self) {
+        unsafe {
+            (*self.parent).update_shifters();
+        }
+    }
 }
 
 #[derive(Clone)]
-pub struct ShifterValueRange {
+pub struct ShifterValueRange<const N: usize, OP: OutputPin> {
     inner: *mut [u8],
-    len: usize,
+    //len: usize,
+    parent: *mut AdvancedShiftRegister<N, OP>,
 }
 
-impl ShifterValueRange {
+impl<const N: usize, OP: OutputPin> ShifterValueRange<N, OP> {
     pub fn set_data(&self, data: &[u8]) {
         unsafe {
             let ptr = &mut *self.inner;
@@ -101,6 +94,12 @@ impl ShifterValueRange {
         unsafe {
             let ptr = &mut *self.inner;
             ptr[index] = value;
+        }
+    }
+
+    pub fn update_shifters(&self) {
+        unsafe {
+            (*self.parent).update_shifters();
         }
     }
 }
