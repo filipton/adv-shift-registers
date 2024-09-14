b8d499cd0a5e2dc2f9c65e238779f32d02e6e66a
filipton <filipton12@gmail.com>
2024-09-14 21:16:01 +0200

    explore: addr_of_mut

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,14 @@ impl<const N: usize, OP: OutputPin> AdvancedShiftRegister<N, OP> {
         }
     }
 
+    pub fn get_shifter_mut(&mut self, i: usize) -> *mut u8 {
+        if i >= N {
+            panic!("i too large!");
+        }
+
+        core::ptr::addr_of_mut!(self.shifters[i])
+    }
+
     pub fn shift_test(&mut self, mut val: u8, latch: bool) {
         for _ in 0..8 {
             let state = PinState::from(val & 1 > 0);
@@ -33,5 +41,23 @@ impl<const N: usize, OP: OutputPin> AdvancedShiftRegister<N, OP> {
             _ = self.latch_pin.set_high();
             _ = self.latch_pin.set_low();
         }
+    }
+
+    pub fn shift_test_2(&mut self) {
+        for i in (0..N).rev() {
+            let mut val = self.shifters[i];
+
+            for _ in 0..8 {
+                let state = PinState::from(val & 1 > 0);
+                _ = self.data_pin.set_state(state);
+                val >>= 1;
+
+                _ = self.clk_pin.set_high();
+                _ = self.clk_pin.set_low();
+            }
+        }
+
+        _ = self.latch_pin.set_high();
+        _ = self.latch_pin.set_low();
     }
 }
